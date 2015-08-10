9462bb627f13f25914076ab18674eee7fb0dcad2
kuga <kuga@ONEPIECE.com>
2015-08-11 01:34:33 +0800

    improve comment

--- miller-rabin_en.c
+++ miller-rabin_en.c
@@ -1,36 +1,20 @@
 /*
-    miller-rabin test base on BigInt
-    update: 2014.06.09 by xkuga
+    Miller-Rabin test
     
-    function: GenPrime(bitLen, times, result)
-    generate prime by specify bit length and miller-rabin test times
-    generate a 1024bit prime may need about 6 hours
-    generate a 512bit prime may need about 3 hours
-    generate a 100bit prime may need about 1 minute
-    generate a 32bit prime may need about 4 seconds
-    if you generate a big prime, make sure the BIG_INT_BIT_LEN is enough
+    Function: GenPrime(bitLen, times, result)
+    bitlen: bit length, times: times of Miller-Rabin test   
+    If you generate a big prime, make sure the BIG_INT_BIT_LEN is enough
+    
+    1024 bit: about 6 hours
+    512 bit: about 3 hours
+    100 bit: about 1 minute
+    32 bit: about 4 seconds
 
-    notice:
-    the generate is not random
-    it first test the biggest BigInt, it means all bit is 1
-    then it subtract 2, and test again, unitl it find the prime
-    so it aways generate the biggest prime in the specify bit length
-    of course you can generate it randomly, good luck ><
-
-    BigInt operation
-
-    support operation: +, -, *, /, ^, %, ^%
-    the program is implement by binary
-    the BigInt use complement to store and calculate
-    multiplication: implement by using booth algorithm
-    division: implement by using binary search
-    pow and powmod: implement by using binary way
-
-    maybe the program is not very fast, but it works anyway.
-    what's more, it's easy to learn and understand how the computer works
-    the full program can download on Github: https://github.com/xkuga/miller-rabin
-    if you find any mistakes, you can contact me on weibo(http://weibo.com/cpl86)
-    thank you
+    Notice:
+    The generation is not random.
+    It first test the biggest BigInt, namely all bit is 1.
+    Then it subtract 2 and test again unitl it find the prime.
+    So it aways generate the biggest prime.
 */
 
 #include <stdio.h>
